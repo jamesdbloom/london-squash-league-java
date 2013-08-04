f3611134a27a02fd773b1ea4280de2cb91428469
James Bloom <jamesdbloom@gmail.com>
2013-08-04 07:01:08 +0100

    adding daily DB backups

---
+++ src/main/resources/db_backup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# To install use the following cron job:
+#
+# 0 0 * * * bash /var/apps/london_squash_league/london_squash_league-1.0-SNAPSHOT/WEB-INF/classes/db_backup.sh
+#
+
+mysqldump --single-transaction -u squash_backup --password=itchyfr3dsm1th -h localhost squash_league > "/var/db_backup/london_squash_league/squash_league_$(/bin/date +%y-%m-%d-%H-%M-%s).sql"
